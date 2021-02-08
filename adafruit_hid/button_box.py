970a4b595ac08a14ddee59100e29578df75dbdb0
Jason Dilworth <hello@jasondilworth.co.uk>
2021-02-08 21:12:59 +0000

    Attempt to create button_box

--- adafruit_hid/button_box.py
+++ adafruit_hid/button_box.py
@@ -3,10 +3,10 @@
 # SPDX-License-Identifier: MIT
 
 """
-`adafruit_hid.gamepad.Gamepad`
+`adafruit_hid.button_box.ButtonBox`
 ====================================================
 
-* Author(s): Dan Halbert
+* Author(s): Dan Halbert, Jason Dilworth
 """
 
 import struct
@@ -15,33 +15,25 @@ import time
 from . import find_device
 
 
-class Gamepad:
-    """Emulate a generic gamepad controller with 16 buttons,
-    numbered 1-16, and two joysticks, one controlling
-    ``x` and ``y`` values, and the other controlling ``z`` and
-    ``r_z`` (z rotation or ``Rz``) values.
-
-    The joystick values could be interpreted
-    differently by the receiving program: those are just the names used here.
-    The joystick values are in the range -127 to 127."""
+class ButtonBox:
+    """Emulate a generic gamepad controller with 32 buttons,
+    numbered 1-32."""
 
     def __init__(self, devices):
-        """Create a Gamepad object that will send USB gamepad HID reports.
+        """Create a ButtonBox object that will send USB gamepad HID reports.
 
-        Devices can be a list of devices that includes a gamepad device or a gamepad device
+        Devices can be a list of devices that includes a button_box device or a button_box device
         itself. A device is any object that implements ``send_report()``, ``usage_page`` and
         ``usage``.
         """
-        self._gamepad_device = find_device(devices, usage_page=0x1, usage=0x05)
+        self._button_box_device = find_device(devices, usage_page=0x1, usage=0x04)
 
         # Reuse this bytearray to send mouse reports.
         # Typically controllers start numbering buttons at 1 rather than 0.
         # report[0] buttons 1-8 (LSB is button 1)
         # report[1] buttons 9-16
-        # report[2] joystick 0 x: -127 to 127
-        # report[3] joystick 0 y: -127 to 127
-        # report[4] joystick 1 x: -127 to 127
-        # report[5] joystick 1 y: -127 to 127
+        # report[2] buttons 17-24
+        # report[3] buttons 25-32
         self._report = bytearray(6)
 
         # Remember the last report as well, so we can avoid sending
@@ -51,10 +43,6 @@ class Gamepad:
         # Store settings separately before putting into report. Saves code
         # especially for buttons.
         self._buttons_state = 0
-        self._joy_x = 0
-        self._joy_y = 0
-        self._joy_z = 0
-        self._joy_r_z = 0
 
         # Send an initial report to test if HID device is ready.
         # If not, wait a bit and try once more.
@@ -87,41 +75,9 @@ class Gamepad:
         self.press_buttons(*buttons)
         self.release_buttons(*buttons)
 
-    def move_joysticks(self, x=None, y=None, z=None, r_z=None):
-        """Set and send the given joystick values.
-        The joysticks will remain set with the given values until changed
-
-        One joystick provides ``x`` and ``y`` values,
-        and the other provides ``z`` and ``r_z`` (z rotation).
-        Any values left as ``None`` will not be changed.
-
-        All values must be in the range -127 to 127 inclusive.
-
-        Examples::
-
-            # Change x and y values only.
-            gp.move_joysticks(x=100, y=-50)
-
-            # Reset all joystick values to center position.
-            gp.move_joysticks(0, 0, 0, 0)
-        """
-        if x is not None:
-            self._joy_x = self._validate_joystick_value(x)
-        if y is not None:
-            self._joy_y = self._validate_joystick_value(y)
-        if z is not None:
-            self._joy_z = self._validate_joystick_value(z)
-        if r_z is not None:
-            self._joy_r_z = self._validate_joystick_value(r_z)
-        self._send()
-
     def reset_all(self):
         """Release all buttons and set joysticks to zero."""
         self._buttons_state = 0
-        self._joy_x = 0
-        self._joy_y = 0
-        self._joy_z = 0
-        self._joy_r_z = 0
         self._send(always=True)
 
     def _send(self, always=False):
@@ -133,25 +89,15 @@ class Gamepad:
             self._report,
             0,
             self._buttons_state,
-            self._joy_x,
-            self._joy_y,
-            self._joy_z,
-            self._joy_r_z,
         )
 
         if always or self._last_report != self._report:
-            self._gamepad_device.send_report(self._report)
+            self._button_box_device.send_report(self._report)
             # Remember what we sent, without allocating new storage.
             self._last_report[:] = self._report
 
     @staticmethod
     def _validate_button_number(button):
-        if not 1 <= button <= 16:
-            raise ValueError("Button number must in range 1 to 16")
+        if not 1 <= button <= 32:
+            raise ValueError("Button number must in range 1 to 32")
         return button
-
-    @staticmethod
-    def _validate_joystick_value(value):
-        if not -127 <= value <= 127:
-            raise ValueError("Joystick value must be in range -127 to 127")
-        return value
